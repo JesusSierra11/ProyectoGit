7bd5816ef30ca4c46bf55b2475e002558d599784
JesusSierra11 <79005018+JesusSierra11@users.noreply.github.com>
2021-03-21 14:34:06 +0100

    Trabajo finalizado

--- Clases/ThreeDimensionalShape.java
+++ Clases/ThreeDimensionalShape.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.ProyectoGit;
 
 /**
  * Esta es la clase Forma tridimensional. Es una clase abstracta. Esta clase
